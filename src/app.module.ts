fdda8cfdf53f8ce29e885bdb6765cd29e66fbaf8
mario <mario.mazzarelli@asfweb.net>
2023-02-12 17:24:54 +0100

    done - docker build

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/db'),
-    UsersModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
